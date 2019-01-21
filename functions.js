ffd1d817f721cf152299ecaa2b812847ccfd54f5
Peter-S <32533746+Peter-S@users.noreply.github.com>
2019-01-21 15:33:44 -0800

    HomeWork Commit

---
+++ functions.js
@@ -0,0 +1,27 @@
+function myFunction1() {
+	alert ("You Clicked |WTF|");
+}
+
+function myFunction2() {
+	document.body.style.backgroundColor = "chartreuse";
+}
+
+function myFunction3() {
+	document.body.style.backgroundColor = "white";
+}
+
+function myFunction4() {
+	document.body.style.backgroundColor = "red";
+}
+
+function myFunction5() {
+	document.getElementById("time").innerHTML = Date();
+}
+
+function myFunction6(box) {
+	box.style.background="yellow";
+}
+
+function myFunction7() {
+	alert("You pressed a Key");
+}
